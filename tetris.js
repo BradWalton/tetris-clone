3ac62d5900ec2b60a48bbf60ee98708960573215
BradWalton <walton4747@gmail.com>
2016-03-16 21:29:24 -0300

    set up game canvas with squares

--- tetris.js
+++ tetris.js
@@ -1,5 +1,29 @@
 /**
  * Created by Brad on 2016-03-15.
  */
-var canvas = document.getElementById("canvas");
+var SQ      = 25;   // square side in pixels
+var HCOUNT  = 16;   // horizontal width in squares
+var VCOUNT  = 26;   // vertical width in squares
+var WIDTH   = SQ * HCOUNT;
+var HEIGHT  = SQ * VCOUNT;
+
+var canvas = document.getElementById("game");
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 var ctx = canvas.getContext('2d');
+
+// Object definitions
+// Bridge, Cube, Left L, Right L, S shape, Z shape, T shape, Pipe (I)
+// '0' is the normal orientation
+// '1' is the object rotated 90 degrees clockwise
+// '2' is the object rotated 180 degrees clockwise
+// '3' is the object rotated 270 degrees clockwise
+
+var objects = [
+    {//Bridge
+        fill: '#FFBF00',
+        0:[], 1:[],
+        2:[], 3:[]
+    }
+
+];
